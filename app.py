d44cbfbffb44a726d5f1ed54a6c19dfc6db37579
deneve <denevee@proton.me>
2024-06-28 19:10:14 +0300

    create Flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pandas as pd
+from data_processing import load_and_process_data
+from visualization import visualize_temperature_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data', methods=['POST'])
+def get_data():
+    file = request.files['file']
+    file_path = f'data/{file.filename}'
+    file.save(file_path)
+
+    data = load_and_process_data(file_path)
+    visualize_temperature_data(data)
+
+    return render_template('data.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
