1d6b61d0fcbba61f99c26e4916bae19d835be9fe
deneve <denevee@proton.me>
2024-07-06 05:42:19 +0300

    create initial migration / refactor the app

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from app import create_app
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = create_app()
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Station(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    station_code = db.Column(db.String(50), unique=True, nullable=False)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    elevation = db.Column(db.Float)
+    name = db.Column(db.String(50))
+
+class TemperaturRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    station_id = db.Column(db.Integer, db.ForeignKey('station.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    tmax = db.Column(db.Float)
+    tmin = db.Column(db.Float)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    data = TemperaturRecord.query.all()
+    # return jsonify([d.to_dict() for d in data])
+    return render_template('data.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
