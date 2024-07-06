edd8a04a322ab6a6aae4ff01a65120589efae8cf
deneve <denevee@proton.me>
2024-07-06 05:23:38 +0300

    update requirements

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+from dotenv import load_dotenv
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'climate_data.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLACLHEMY_TRACK_MODIFICATIONS = False
